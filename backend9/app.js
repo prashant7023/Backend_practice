25e62088eb5a2c45e5e4ad3f30427e41da42f7e5
Prashant Singh <prashantsh7014@gmail.com>
2025-02-14 16:21:20 +0530

    update backend 9 and finish

--- backend9/app.js
+++ backend9/app.js
@@ -35,8 +35,18 @@ app.get('/like/:id' , isLoggedin , async (req, res) => {
   }else{
     post.likes.splice(post.likes.indexOf(req.user.userid), 1);
   }
-
   await post.save();
+  res.redirect('/profile');
+});
+
+app.get('/edit/:id' , isLoggedin , async (req, res) => {
+  let post = await postModel.findOne({_id: req.params.id}).populate('user');
+
+  res.render('edit', {post});
+});
+app.post('/update/:id' , isLoggedin , async (req, res) => {
+  let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content});
+
   res.redirect('/profile');
 });
 
